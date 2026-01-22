8b94f1955283ad4fba1cb552cd40f1f21e2d4124
Harshit Jaiswal <111104708+storm1357@users.noreply.github.com>
2026-01-22 14:56:06 +0530

    Update 3507_Minimum_Pair_Removal_to_Sort_Array_I.cpp

--- 3507_Minimum_Pair_Removal_to_Sort_Array_I.cpp
+++ 3507_Minimum_Pair_Removal_to_Sort_Array_I.cpp
@@ -1,5 +1,16 @@
 class Solution {
 public:
+struct Paircom{
+    bool operator()(const Pair* a, Pair* b)const{
+        if(a->right == nullptr || b->right == nullptr){
+            return a->right == nullptr;
+        }
+        long long diff = a->sum - b->sum;
+        if(diff = 0)
+            return diff < 0;
+        return a->idx < b->idx;
+    }
+}
     int minimumPairRemoval(vector<int>& nums) {
         
     }
